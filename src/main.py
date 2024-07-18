70b5b8ac84a4e426be946699bf4e168a8c168964
namtoptall <s3877256@rmit.edu.vn>
2024-07-18 19:34:11 +0700

    quen con me no push :))

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
+from pathlib import Path
 from processing import generate_report
 
-input_file = 'data/test-file.vtt' 
-output_file = 'results/meeting_summary.txt'  
-reference_summary = 'data/references_summary.txt'
+def main():
+    input_file = 'data/test-file.vtt'
+    output_file = 'results/meeting_summary.txt'
+    reference_summary_file = 'data/references_summary.txt'
 
-generate_report(input_file, output_file, reference_summary)
+    input_path = Path(input_file)
+    output_path = Path(output_file)
+    reference_summary_path = Path(reference_summary_file)
 
-# Load and print the generated summary
-with open(output_file, 'r', encoding='utf-8') as file:
-    summary = file.read()
-    print(summary)
+    if not input_path.is_file():
+        print(f"Error: The input file {input_file} does not exist.")
+        return
+
+    reference_summary = reference_summary_path if reference_summary_path.is_file() else None
+    if reference_summary is None:
+        print(f"Warning: The reference summary file {reference_summary_file} does not exist. Skipping BLEU score calculation.")
+
+    bleu_score = generate_report(str(input_path), str(output_path), str(reference_summary) if reference_summary else None)
+if __name__ == "__main__":
+    main()
--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,13 @@
 import re
-import json
+import nltk
+from pathlib import Path
 from langchain_aws import BedrockLLM
 from langchain.chains import ConversationChain
-from langchain.memory.buffer import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
-from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION,MODEL_ID
-
+from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, MODEL_ID
 
 def preprocess_file(vtt_file, output_file):
-    # open the vtt file and read the lines
     with open(vtt_file, 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
@@ -20,10 +19,10 @@ def preprocess_file(vtt_file, output_file):
 
     for line in lines:
         line = line.strip()
-        
+
         if re.match(r'\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/\d+-\d+', line):
             continue
-        # match the time range using regex 
+
         time_match = re.match(r'(\d{2}:\d{2}:\d{2}\.\d{3}) --> (\d{2}:\d{2}:\d{2}\.\d{3})', line)
         if time_match:
             if time_range and speaker and text:
@@ -32,7 +31,7 @@ def preprocess_file(vtt_file, output_file):
             speaker = None
             text = []
             continue
-        # match the speaker using regex
+
         speaker_match = re.match(r'<v\s([^>]+)>(.+)', line)
         if speaker_match:
             if speaker and text:
@@ -44,40 +43,43 @@ def preprocess_file(vtt_file, output_file):
 
         if line and time_range:
             text.append(line.replace('</v>', '').strip())
-    # append the last conversation to the conversation list
+
     if time_range and speaker and text:
         conversation.append(f"{speaker}|{time_range}|{' '.join(text)}")
 
     with open(output_file, 'w', encoding='utf-8') as file:
-        file.write("\n".join(conversation)) # viet vao thoi gian
+        file.write("\n".join(conversation))
 
     return attendees
+
 
 def save_to_file(content, file_path):
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(content)
+
 
 def load_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
     return content
 
+
 def summarize_chunk(chunk):
-    #create a bedrock instance 
     llm = BedrockLLM(
         region_name=AWS_REGION,
         model_id=MODEL_ID,
     )
     prompt_template = """ 
-    Develop comprehensive meeting minutes including:
-    Attendees: List all participants along with their roles and departments.
-    Discussion Points: Detail the topics discussed, including any debates or alternate viewpoints.
-    Decisions Made: Record all decisions, including who made them and the rationale.
-    Action Items: Specify tasks assigned, responsible individuals, and deadlines.
-    Data & Insights: Summarize any data presented or insights shared that influenced the meeting's course.
-    Follow-Up: Note any agreed-upon follow-up meetings or checkpoints.
-    Furhermore, please do no response to this prompt but only generate a sumaary of the following meeting minutes
-    in no more than 350 words: 
+    Please act as a meeting assistant,develop comprehensive meeting summarization including:
+    - Attendees: List all participants along with their roles and departments.
+    - Discussion Points: Summarize the key topics discussed.
+    - Decisions Made: Record the main decisions taken.
+    - Action Items: Specify tasks assigned, responsible individuals, and deadlines.
+    - Follow-Up: Mention any agreed-upon follow-up meetings or checkpoints.
+    
+    Please generate a summary of the following meeting minutes in no more than 300 words. 
+    Moreover, Avoid repeating information. 
+    Format the summary as a report for me.
     {text}"""
     prompt = prompt_template.format(text=chunk)
 
@@ -89,65 +91,38 @@ def summarize_chunk(chunk):
     return summary
 
 
-def split_text(text, max_tokens):
-    # split the text into 4 parts 
+def split_text(text, max_tokens, overlap_tokens):
     words = text.split()
-    num_tokens = len(words)
-    tokens_per_part = num_tokens // 4
-
     chunks = []
-    for i in range(4):
-        start_idx = i * tokens_per_part
-        end_idx = (i + 1) * tokens_per_part if i < 3 else num_tokens
+    start_idx = 0
+
+    while start_idx < len(words):
+        end_idx = min(start_idx + max_tokens, len(words))
         chunk = ' '.join(words[start_idx:end_idx])
         chunks.append(chunk)
+        start_idx += max_tokens - overlap_tokens
 
     return chunks
 
+
 def summarize_text(text):
     MAX_TOKENS = 8192
-    OUTPUT_MAX_TOKENS = 250
-    chunks = split_text(text, MAX_TOKENS - OUTPUT_MAX_TOKENS)
+    OVERLAP_TOKENS = 1024
+    # OUTPUT_MAX_TOKENS = 250
+
+    chunks = split_text(text, MAX_TOKENS, OVERLAP_TOKENS)
     summaries = [summarize_chunk(chunk) for chunk in chunks]
-    
+
     unique_summaries = []
     seen_sentences = set()
+    # Remove duplicate sentences
     for summary in summaries:
         sentences = summary.split('. ')
         filtered_sentences = [s for s in sentences if s and s not in seen_sentences]
         seen_sentences.update(filtered_sentences)
         unique_summaries.append('. '.join(filtered_sentences))
-    
+
     return ' '.join(unique_summaries)
-
-def calculate_bleu_score(reference, hypothesis):
-    def preprocess_text(text):
-        text = text.lower()
-        text = re.sub(r'[^\w\s]', '', text)
-        return text
-
-    reference = preprocess_text(reference)
-    hypothesis = preprocess_text(hypothesis)
-
-    reference_tokens = [reference.split()]
-    hypothesis_tokens = hypothesis.split()
-    smoothing_function = SmoothingFunction().method1
-    bleu_score = sentence_bleu(reference_tokens, hypothesis_tokens, smoothing_function=smoothing_function)
-    return bleu_score
-
-def generate_report(input_file, output_file, reference_summary=None):
-    attendees = preprocess_file(input_file, output_file)
-    text = load_from_file(output_file)
-    summary = summarize_text(text)
-    
-    formatted_summary = format_summary_as_report(summary, attendees)
-    
-    save_to_file(formatted_summary, output_file)
-    
-    if reference_summary:
-        reference_text = reference_summary
-        bleu_score = calculate_bleu_score(reference_text, formatted_summary)
-        print(f"BLEU score: {bleu_score}")
 
 def format_summary_as_report(summary, attendees):
     report_template = '''
@@ -159,12 +134,22 @@ def format_summary_as_report(summary, attendees):
     **Summary:**
     {summary}
     '''
-    
+
     attendees_list = ', '.join(attendees)
     attendees_count = len(attendees)
-    
     return report_template.format(
-        attendees_count=attendees_count, 
-        attendees_list=attendees_list, 
-        summary=summary
+        attendees_count=attendees_count,
+        attendees_list=attendees_list,
+        summary=summary,
+        
     )
+    
+def generate_report(input_file, output_file, reference_summary=None):
+    attendees = preprocess_file(input_file, output_file)
+    text = load_from_file(output_file)
+    summary = summarize_text(text)
+
+    formatted_summary = format_summary_as_report(summary, attendees)
+
+    save_to_file(formatted_summary, output_file)
+    
